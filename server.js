7305513a945948469d2cdf1af7baa43453cb823c
Andreas Reiterer <reiterer.a@gmail.com>
2017-12-09 15:32:50 +0100

    create express app

--- server.js
+++ server.js
@@ -3,3 +3,8 @@ const MongoClient = require("mongodb").MongoClient;
 const bodyParser = require("body-parser");
 
 const app = express();
+const port = 8000;
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+});
