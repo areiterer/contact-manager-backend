94019bf3d8daf66212a3cc37b68b4531156425cb
Andreas Reiterer <reiterer.a@gmail.com>
2017-12-11 18:56:23 +0100

    add lowdb as local JSON database

--- server.js
+++ server.js
@@ -7,8 +7,17 @@ const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+
+const adapter = new FileSync("db.json");
+const db = low(adapter);
+
+// Set some defaults
+db.defaults({ contacts: [] }).write();
+
 // Routes
-require("./app/routes")(app, {});
+require("./app/routes")(app, db);
 
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
