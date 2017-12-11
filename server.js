e9b526f7e2acbd70d7a41569a2706cca847a56c0
Andreas Reiterer <reiterer.a@gmail.com>
2017-12-11 20:52:24 +0100

    Add CORS headers

--- server.js
+++ server.js
@@ -15,6 +15,15 @@ low(adapter).then(db => {
   // Set some defaults
   db.defaults({ contacts: [] }).write();
 
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+
   // Routes
   require("./app/routes")(app, db);
 
