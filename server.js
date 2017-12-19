5513f060836d88e30e156b5b3c4d16201eaa4c9a
Andreas Reiterer <reiterer.a@gmail.com>
2017-12-19 15:53:07 +0100

    add bodyParser.json + improve CORS

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 8000;
 
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const low = require("lowdb");
@@ -15,14 +16,23 @@ low(adapter).then(db => {
   // Set some defaults
   db.defaults({ contacts: [] }).write();
 
-  app.use(function(req, res, next) {
+  // ## CORS middleware
+  //
+  // see: http://stackoverflow.com/questions/7067966/how-to-allow-cors-in-express-nodejs
+  var allowCrossDomain = function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+    // intercept OPTIONS method
+    if ("OPTIONS" == req.method) {
+      res.send(200);
+    } else {
+      next();
+    }
+  };
+
+  app.use(allowCrossDomain);
 
   // Routes
   require("./app/routes")(app, db);
